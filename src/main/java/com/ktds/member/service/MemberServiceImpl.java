2e144f0063506ddb6b745971432595b000fe6ab3
DESKTOP-05VV0CS\KKJ <rudwnsdl93@naver.com>
2018-09-24 19:26:15 +0900

    파일 업로드

--- src/main/java/com/ktds/member/service/MemberServiceImpl.java
+++ src/main/java/com/ktds/member/service/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package com.ktds.member.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ktds.common.util.SHA256Util;
 import com.ktds.member.dao.MemberDao;
 import com.ktds.member.vo.MemberVO;
 
@@ -14,7 +15,16 @@ public class MemberServiceImpl implements MemberService {
 
 	@Override
 	public boolean createMember(MemberVO memberVO) {
+		String salt = SHA256Util.generateSalt();
+		String password = this.getHashedPassrod(salt, memberVO.getPassword());
+		
+		memberVO.setPassword(password);
+		memberVO.setSalt(salt);		
 		return this.memberDao.insertMember(memberVO) > 0;
+	}
+	
+	public String getHashedPassrod(String salt, String password) {
+		return SHA256Util.getEncrypt(password, salt);
 	}
 	
 	@Override
